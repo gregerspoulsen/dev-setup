886a8d075057157bf998251d7661ee0e086fe9cf
Gregers Poulsen <gp@airborneinstruments.dk>
2024-10-02 21:35:46 +0200

    Still working

--- system-config/sway.nix
+++ system-config/sway.nix
@@ -4,13 +4,16 @@
 
 {
   security.polkit.enable = true;
-  programs.light.enable = true;
-  xdg.portal = {
-    enable = true;
-    wlr.enable = true;
-  };
-  hardware.opengl = {
-    enable = true;
+  # programs.light.enable = true;
+  # xdg.portal = {
+  #   enable = true;
+  #   wlr.enable = true;
+  # };
+  # hardware.opengl = {
+  #   enable = true;
+  # };
+  environment.sessionVariables = {
+    WLR_NO_HARDWARE_CURSORS = "1";
   };
 }
 
--- user/sway.nix
+++ user/sway.nix
@@ -2,17 +2,17 @@
 
 {
 
-  xdg.enable = true;
+  # xdg.enable = true;
   wayland.windowManager.sway = {
     enable = true;
     config = rec {
-      startup = [
-        # https://github.com/NixOS/nixpkgs/issues/119445
-        {command = "dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK";}
-      ];
+      # startup = [
+      #   # https://github.com/NixOS/nixpkgs/issues/119445
+      #   {command = "dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK";}
+      # ];
       modifier = "Mod4";
       # Use kitty as default terminal
-      terminal = "kitty"; 
+      terminal = "alacritty"; 
     };
   };
 }
